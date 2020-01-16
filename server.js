c076e43017b6a2c622e03b032ff033518e58b657
dvoineu <av.dvoinev@gmail.com>
2020-01-16 13:23:02 +0100

    Routes and middleware added

--- server.js
+++ server.js
@@ -1,9 +1,23 @@
 const express = require('express');
+const morgan = require('morgan');
 const dotenv = require('dotenv');
+const logger = require('./middleware/logger');
 
-dotenv.config({ path: './config/confing.env' });
+// Route files
+const bootcamps = require('./routes/bootcamps');
+
+// Load env vars
+dotenv.config({ path: './config/config.env' });
 
 const app = express();
+
+// Dev logging middleware
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
+
+// Mount routers
+app.use('/api/v1/bootcamps', bootcamps);
 
 const PORT = process.env.PORT || 5000;
 
