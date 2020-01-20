3bf295949382f4d76e431502f0fd6fe3c57fd2bd
dvoineu <av.dvoinev@gmail.com>
2020-01-20 12:47:13 +0100

    Error Handling added

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const morgan = require('morgan');
+const colors = require('colors');
 const dotenv = require('dotenv');
+const errorHandler = require('./middleware/error');
 const logger = require('./middleware/logger');
+const connectDB = require('./config/db');
 
 // Route files
 const bootcamps = require('./routes/bootcamps');
@@ -9,7 +12,13 @@ const bootcamps = require('./routes/bootcamps');
 // Load env vars
 dotenv.config({ path: './config/config.env' });
 
+// Connect to database
+connectDB();
+
 const app = express();
+
+// Body parser
+app.use(express.json());
 
 // Dev logging middleware
 if (process.env.NODE_ENV === 'development') {
@@ -19,9 +28,22 @@ if (process.env.NODE_ENV === 'development') {
 // Mount routers
 app.use('/api/v1/bootcamps', bootcamps);
 
+// This middleware should be at the end in order to work
+app.use(errorHandler);
+
 const PORT = process.env.PORT || 5000;
 
-app.listen(
+const server = app.listen(
   PORT,
-  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
+  console.log(
+    `Server running in ${process.env.NODE_ENV} mode on port ${PORT}.`.yellow
+      .bold
+  )
 );
+
+// Handle unhandle promise rejections
+process.on('unhandledRejection', (err, promise) => {
+  console.log(`Error: ${err.message}`.red);
+  // Close server and exit
+  server.close(() => process.exit(1));
+});
